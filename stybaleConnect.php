231c83befa161c83187f6bcdeef1e012e998ecc0
Ali SABOURI <72344723+alisabourii@users.noreply.github.com>
2024-06-19 11:09:53 +0300

    Add files via upload

---
+++ stybaleConnect.php
@@ -0,0 +1,28 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "test1";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$bgroup = $_POST['bgroup'];
+
+$sql = "INSERT INTO users (name, email, phone, bgroup) VALUES ('$name', '$email', '$phone','$bgroup')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
