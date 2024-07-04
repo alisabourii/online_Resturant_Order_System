2e0664397c4c75be5a884004e26c125da266419b
Ali SABOURI <72344723+alisabourii@users.noreply.github.com>
2024-07-04 20:13:57 +0300

    Add files via upload

--- hesaplar.php
+++ hesaplar.php
@@ -6,7 +6,6 @@
         <title>Document</title>
         <link rel="stylesheet" href="style.css">
 </head>
-
 <?php 
 $servername = "localhost";
 $username = "root";
@@ -20,35 +19,52 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT * from resturanttable WHERE tarih > '2024-07-01' ";
+$sql = "SELECT * from resturanttable";
 $result = $conn->query($sql);
 
-echo"Sipareş ID-------Tarih ---------------------Sipareş -------------------------Toplam Fiyat";
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo "<br>------" .$row["id"]. "------". $row["tarih"]. " " . $row["orders"] . $row["totoalPrice"]."<br>";
-    }
-} else {
-    echo "0 results";
+function dayli(){
+    $sql = "SELECT * from resturanttable WHERE tarih < '2024-06-30'";
 }
-
-$conn->close();
-?>
-
 ?>
 
 <body>
-        <Button type="sumbit" name='sumbit' id="sumbit" style="margin-left: 7%; margin-top: 10%;" class="glow-on-hover">Gün</Button>
+        <Button type="sumbit" name='sumbit' id="sumbit" style="margin-left: 7%; margin-top: 5%;" class="glow-on-hover">Gün</Button>
         <Button type="sumbit" name='sumbit' id="sumbit" style="margin-left: 4%; margin-top: 10%;" class="glow-on-hover">Hafta</Button>
         <Button type="sumbit" name='sumbit' id="sumbit" style="margin-left: 4%; margin-top: 10%;" class="glow-on-hover">Ay</Button>
 
         <Button type="sumbit" name='sumbit' id="sumbit" style="margin-left: 4%; margin-top: 10%;" class="glow-on-hover" style="width: 40%;">Yıllık</Button>
 
+        <br><br>
+
+        <table border='1' style="text-align:center;">
+        <thead>    <!--Table Head-->
+            <th>Tarih</th>   <!--Table Heading-->
+            <th>Sipareşler</th> 
+            <th>Toplam turari</th> 
+        </thead>
+        <tbody>     <!--Table Body-->
+        <?php 
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+            echo "<tr>";
+            echo "<td>" . $row["tarih"];
+            echo "<td>". $row["orders"];
+            echo "<td>". $row["totoalPrice"]."<br>"."<br>";
+        }
+    } else {
+        echo "0 results";
+    }
+    
+    $conn->close();
+    ?>
+        </tbody>
+        <tfooter>
+        </tfooter>
+    </table>
+    
 </body>
 
-<?php 
 
-?>
 
 </html>
