21e56eb97badc6e425fc69bb5cd98add14044b4d
Ali SABOURI <72344723+alisabourii@users.noreply.github.com>
2024-07-05 22:41:59 +0300

    Add files via upload

--- hesaplar.php
+++ hesaplar.php
@@ -6,37 +6,22 @@
         <title>Document</title>
         <link rel="stylesheet" href="style.css">
 </head>
-<?php 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "test1";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-$sql = "SELECT * from resturanttable";
-$result = $conn->query($sql);
-
-function dayli(){
-    $sql = "SELECT * from resturanttable WHERE tarih < '2024-06-30'";
-}
-?>
 
 <body>
-        <Button type="sumbit" name='sumbit' id="sumbit" style="margin-left: 7%; margin-top: 5%;" class="glow-on-hover">Gün</Button>
-        <Button type="sumbit" name='sumbit' id="sumbit" style="margin-left: 4%; margin-top: 10%;" class="glow-on-hover">Hafta</Button>
+    <Button  class="glow-on-hover" style="margin-left: 30%; width: 40%;" onclick="location.href='index.php'">Ana Sayfa</Button>
+    <form action="" method="post" >
+        <Button type="sumbit" name='sumbitGun' id="sumbitGun" style="margin-left: 7%;" class="glow-on-hover" onclick="<?php sqlHesapTable("SELECT * from resturanttable WHERE tarih > '2024-06-30'"); ?>">Gün</Button>
+
+        <Button type="sumbit" name='sumbitHafta' id="sumbitHafta" style="margin-left: 4%; margin-top: 10%;" class="glow-on-hover" onclick="<?php sqlHesapTable("SELECT * from resturanttable"); ?>">Hafta</Button>
+
         <Button type="sumbit" name='sumbit' id="sumbit" style="margin-left: 4%; margin-top: 10%;" class="glow-on-hover">Ay</Button>
 
         <Button type="sumbit" name='sumbit' id="sumbit" style="margin-left: 4%; margin-top: 10%;" class="glow-on-hover" style="width: 40%;">Yıllık</Button>
-
+    </form>
         <br><br>
 
-        <table border='1' style="text-align:center;">
+        <table border='5' style="text-align:center;">
         <thead>    <!--Table Head-->
             <th>Tarih</th>   <!--Table Heading-->
             <th>Sipareşler</th> 
@@ -44,20 +29,44 @@ function dayli(){
         </thead>
         <tbody>     <!--Table Body-->
         <?php 
-    if ($result->num_rows > 0) {
-        // output data of each row
-        while($row = $result->fetch_assoc()) {
-            echo "<tr>";
-            echo "<td>" . $row["tarih"];
-            echo "<td>". $row["orders"];
-            echo "<td>". $row["totoalPrice"]."<br>"."<br>";
+        function sqlHesapTable($sql){      
+            $servername = "localhost";
+            $username = "root";
+            $password = "";
+            $dbname = "test1";
+
+            // Create connection
+            $conn = new mysqli($servername, $username, $password, $dbname);
+            // Check connection
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            }
+
+            $result = $conn->query($sql);
+
+            if ($result->num_rows > 0) {
+                // output data of each row
+                while($row = $result->fetch_assoc()) {
+                    echo "<tr>";
+                    echo "<td>" . $row["tarih"];
+                    echo "<td>". $row["orders"];
+                    echo "<td>". $row["totoalPrice"]."<br>"."<br>";
+                }
+            } else {
+                echo "0 results";
+            }
+            echo"Tamam";
+            $conn->close();
         }
-    } else {
-        echo "0 results";
-    }
-    
-    $conn->close();
-    ?>
+
+        $todayDate = date('Y-m-d', time());
+        if (isset($_POST['sumbitGun'])) {
+            sqlHesapTable("SELECT * from resturanttable WHERE tarih > '2024-06-30'");
+        }
+        else if(isset($_POST["sumbitHafta"])) {
+            sqlHesapTable("SELECT * from resturanttable");
+        }
+        ?>
         </tbody>
         <tfooter>
         </tfooter>
