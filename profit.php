9250e0b2db7fbdebc605f10362371fa7b9c5c132
Ali SABOURI <72344723+alisabourii@users.noreply.github.com>
2024-08-01 12:03:33 +0300

    Add files via upload

--- profit.php
+++ profit.php
@@ -21,10 +21,11 @@
         </style>
 </head>
 <body>
+<Button onclick="myFunc()" style="margin-left: 10%; margin-top: 1%;" class="glow-on-hover" style="width: 40%;">Geri Git</Button>
         <form action="" method="post" style="margin-left: 10%; margin-top:1%; font-size:25px;">
 
-                Tarih: <input type="date" name="dateName" value="<?php echo date("Y-m-d"); ?>"><br><br>
-                
+                Tarih: <input type="date" name="dateName"><br><br>
+
                 <div class="divStyle">Alış: <input type="number" name="alisName" style="width: 30%;"> </div>
 
                 <div class="divStyle">Satış: <input type="number" name="satisName" style="width: 30%;"><br><br></div>
@@ -36,9 +37,35 @@
 
                 <Button type="sumbit" name='bul' id="bul" style="margin-left: 0%; margin-top: 5%;" class="glow-on-hover" style="width: 40%;">Bul</Button>
 
-                <Button type="sumbit" name='sil' id="sil" style="margin-left: 0%; margin-top: 5%;" class="glow-on-hover" style="width: 40%;">Sil</Button>
+                <Button type="sumbit" name='sil' id="sil" style="margin-left: 0%; margin-top: 5%;" class="glow-on-hover" style="width: 40%;">Sil</Button><br>
+
+                <Button type="sumbit" name='karBul' id="karBul" style="margin-left: 0%; margin-top: 5%;" class="glow-on-hover" style="width: 40%;">Bilinmeyen kar</Button>
+
        </form>
+       
         <?php 
+               function totalSels($sql){      
+                $servername = "localhost";
+                $username = "root";
+                $password = "";
+                $dbname = "test1";
+    
+                // Create connection
+                $conn = new mysqli($servername, $username, $password, $dbname);
+                // Check connection
+                if ($conn->connect_error) {
+                    die("Connection failed: " . $conn->connect_error);
+                }
+                $result = $conn->query($sql);
+                if($result->num_rows >0){
+                    $row = $result->fetch_assoc();
+                    $total = $row['Total'];
+                    echo "<h1>Toplam: ". $total ."</h1>";
+                }
+                else{
+                    echo "Sonuç Bulunmadı";
+                }
+            }
                 function ControllerProfits($sql){
                         $servername = "localhost";
                         $username = "root";
@@ -52,34 +79,12 @@
                         }
                        
                         if($conn->query($sql) === TRUE) {
-                                echo '</br>Kayıt/Silme Başarılı';}
+                                echo '</br>Kayıt Başarılı';}
 
                         else{echo '</br> Error: '.$sql."<br>".$conn->error;
                                 echo "</br> </br> </br>";
                                 echo "Her Gün için Sadece bir kere veri ekleyebilirsiniz!";}
                 }
-                function totalSels($sql){      
-                        $servername = "localhost";
-                        $username = "root";
-                        $password = "";
-                        $dbname = "test1";
-            
-                        // Create connection
-                        $conn = new mysqli($servername, $username, $password, $dbname);
-                        // Check connection
-                        if ($conn->connect_error) {
-                            die("Connection failed: " . $conn->connect_error);
-                        }
-                        $result = $conn->query($sql);
-                        if($result->num_rows >0){
-                            $row = $result->fetch_assoc();
-                            $total = $row['Total'];
-                            echo "<h1>Toplam: ". $total ."</h1>";
-                        }
-                        else{
-                            echo "Sonuç Bulunmadı";
-                        }
-                    }
 
                 function showSql($sql){
                         $servername = "localhost";
@@ -102,8 +107,6 @@
                         }
 
                 }
-
-
 
                 if(isset($_POST['kaydet'])){
                         $userInputDate = $_POST['dateName'];  $userBuys = intval($_POST['alisName']);
@@ -136,7 +139,26 @@
                         $userInputDate = $_POST['dateName'];
                         ControllerProfits("DELETE from profhistory WHERE tarih = '".($userInputDate)."'");
                 }
+                else if(isset($_POST['karBul'])){
+                        function foundDate($command){
+                                $date = new DateTime();
+                                $lastMonth = $date->modify("".$command."");
+                                $inputDate = $_POST['dateName'];
+                                return $date->format('Y-m-d');
+                                //return $inputDate;
+                        }
+                        
+                        $trh = $_POST['dateName'];
+                        totalSels("SELECT totoalPrice from resturanttable WHERE tarih > '.$trh.'");
+
+                }
+                
         ?>
 
+        <script>
+                function myFunc() {
+            location.replace('hesaplar.php');
+        }
+        </script>
 </body>
 </html>
