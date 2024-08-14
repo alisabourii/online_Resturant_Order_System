f57513fcfedcb9b023f2638705028be45cac96f6
Ali SABOURI <72344723+alisabourii@users.noreply.github.com>
2024-08-14 12:11:45 +0300

    Add files via upload

--- profit.php
+++ profit.php
@@ -5,6 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Document</title>
         <link rel="stylesheet" href="style.css">
+        <link rel="icon" type="image/x-icon" href="/src/img/ACEIcon.ico">
+
         <style>
                 label{
                         margin-left: 5%;
